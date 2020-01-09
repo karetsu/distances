43c217fc39beeea53532c92a8ddd61183090d2b4
karetsu <raisedbyfinches@hotmail.co.uk>
2020-01-09 14:03:19 +0000

    init: first :fire:

---
+++ shell.nix
@@ -0,0 +1,27 @@
+{ nixpkgs ? import <nixpkgs> { }, compiler ? "default" }:
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, stdenv, base, Cabal, cabal-install, hmatrix, vector }:
+    mkDerivation {
+      pname = "idiom";
+      version = "0.0.0.1";
+      src = ./.;
+      isLibrary = false;
+      isExecutable = true;
+      executableSystemDepends = [ ];
+      executableHaskellDepends = [ base Cabal cabal-install ];
+      homepage = "https://gitlab.com/karetsu/distances";
+      description =
+        "A haskell package for evaluating distances (metrics) between points";
+      license = stdenv.lib.licenses.mit;
+    };
+
+  haskellPackages = if compiler == "default" then
+    pkgs.haskellPackages
+  else
+    pkgs.haskell.packages.${compiler};
+
+  drv = pkgs.lib.id (haskellPackages.callPackage f { });
+in if pkgs.lib.inNixShell then drv.env else drv
